98354a5a3ade745746f831edfbb2a8cb1ed230fc
dodo5517 <kh9690754@naver.com>
2025-04-17 21:40:41 +0900

    feat: CORS 설정

---
+++ src/main/java/me/dodo/readingnotes/config/WebConfig.java
@@ -0,0 +1,25 @@
+package me.dodo.readingnotes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 경로에 대해
+                        .allowedOriginPatterns("http://localhost:3000") // 프론트 도메인 허용
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                        .allowedHeaders("*") // 모든 헤더 허용
+                        .allowCredentials(true); // 쿠키 등 인증 정보 허용
+
+            }
+        };
+    }
+}
