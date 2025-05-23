18fdfe63e8bf3d65f5cc96dc15c9d5c8cfea6cc8
dodo5517 <kh9690754@naver.com>
2025-05-23 21:00:06 +0900

    feat: BookRepository 생성 refactor: ReadingRecordRepository 수정

--- src/main/java/me/dodo/readingnotes/repository/BookRepository.java
+++ src/main/java/me/dodo/readingnotes/repository/BookRepository.java
@@ -1,9 +1,8 @@
 package me.dodo.readingnotes.repository;
 
-import me.dodo.readingnotes.domain.ReadingRecord;
+import me.dodo.readingnotes.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReadingRecordRepository extends JpaRepository<ReadingRecord, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
     // 필요하면 여기에 커스텀 쿼리도 작성
 }
-
