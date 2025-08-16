09392e850c399b1119a2a01ecba1f96dc61515b1
dodo5517 <kh9690754@naver.com>
2025-08-16 23:34:34 +0900

    feat: BookWithLastRecordResponse 생성

---
+++ src/main/java/me/dodo/readingnotes/dto/BookWithLastRecordResponse.java
@@ -0,0 +1,19 @@
+package me.dodo.readingnotes.dto;
+
+import me.dodo.readingnotes.domain.Book;
+
+import java.time.LocalDateTime;
+
+public class BookWithLastRecordResponse extends BookResponse {
+    private LocalDateTime lastRecordAt;
+
+    public BookWithLastRecordResponse(
+            Long bookId, String title, String author, String isbn10,
+            String isbn13, String coverUrl, LocalDateTime lastRecordAt) {
+        super(bookId, author, isbn10, isbn13, coverUrl);
+        this.lastRecordAt = lastRecordAt;
+
+    }
+
+    public LocalDateTime getLastRecordAt() { return lastRecordAt; }
+}
